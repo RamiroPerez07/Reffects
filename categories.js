3c9be01a7feab9dfe5186a3503af3040df06e9b5
RamiroPerez <ramiperez1997@hotmail.com>
2022-11-23 15:30:18 -0300

    agregadas categorias

---
+++ categories.js
@@ -0,0 +1,32 @@
+const Categories = [
+    {
+        id: 1,
+        name: "Compressor",
+        url_img: "./img/categories/c-compressor.png" 
+    },
+    {
+        id: 2,
+        name: "Overdrive",
+        url_img: "./img/categories/c-overdrive.png" 
+    },
+    {
+        id: 3,
+        name: "Distortion",
+        url_img: "./img/categories/c-distortion.png" 
+    },    
+    {
+        id: 4,
+        name: "Modulation",
+        url_img: "./img/categories/c-modulation.png" 
+    },
+    {
+        id: 5,
+        name: "Delay",
+        url_img: "./img/categories/c-delay.png" 
+    },
+    {
+        id: 6,
+        name: "Reverb",
+        url_img: "./img/categories/c-reverb.png" 
+    },
+]
--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 //variables
+let categoryCards = []
 
 //btns
 const barsMenuBtn = document.getElementById("bars-menu-btn")
 
 //menus
 const navbar = document.getElementById("navbar")
+
+//containers
+const categoryContainer = document.getElementById("category-section")
 
 function toggleNavbar(){
     navbar.classList.toggle("show-navbar");
@@ -17,10 +21,60 @@ function closeNavbarMenuOnScroll(){
     barsMenuBtn.classList.remove("open");
 }
 
+function createHtmlCategoryCard(objCategory){
+    return `
+    <div class="category-card">
+        <div class="category-img" style="background-image : url('${objCategory.url_img}')"></div>
+        <span class="category-name">${objCategory.name}</span>
+    </div>
+    `;
+}
+
+function loadCategories(Categories){
+    const html = Categories.map(createHtmlCategoryCard).join("");
+    categoryContainer.innerHTML = html;
+}
+
+function removeSelectedCategory(){
+    console.log(categoryCards)
+    categoryCards.forEach(card =>{
+        console.log(card)
+        if(card.classList.contains("selected")){
+            card.classList.remove("selected");
+        }
+        console.log("no ingreso a la funcion")
+    })
+}
+
+function setStyleOfSelectedCategory(selectedCategoryCard){
+    selectedCategoryCard.classList.add("selected")
+}
+
+function selectCategory(event){
+    if(!event.target.classList.contains("category-card") && !event.target.parentElement.classList.contains("category-card")) return
+    let selectedCategoryCard
+    if (event.target.classList.contains("category-card")){
+        selectedCategoryCard = event.target
+    }else if(event.target.parentElement.classList.contains("category-card")){
+        selectedCategoryCard = event.target.parentElement
+    }
+    removeSelectedCategory();
+    setStyleOfSelectedCategory(selectedCategoryCard);
+}
+
 function init(){
+    window.addEventListener("DOMContentLoaded", function(){
+        loadCategories(Categories);
+        categoryCards = document.getElementsByClassName("category-card");
+        categoryCards = [...categoryCards] //convierto a array de elementos
+    })
+
     barsMenuBtn.addEventListener("click", toggleNavbar);
     window.addEventListener("scroll", closeNavbarMenuOnScroll);
-    navbar.addEventListener("click", toggleNavbar)
+    navbar.addEventListener("click", toggleNavbar);
+
+    categoryContainer.addEventListener("click", selectCategory)
+
 }
 
 init()
