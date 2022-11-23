638bc72e6535b69bcbd8dc540e435ffc3b21645e
RamiroPerez <ramiperez1997@hotmail.com>
2022-11-23 17:01:33 -0300

    completed data

---
+++ data.js
@@ -0,0 +1,116 @@
+const Pedals = [
+    {
+        id:1,
+        name: "Blues Drive BD-2",
+        brand: "Boss",
+        type: ["all","overdrive"],
+        description: "Bluesy sounds with a wide range of nuances",
+        url_img: "./img/pedals/bd2-boss.png",
+        price: 20000,
+    },
+    {
+        id:2,
+        name: "Carbon Copy",
+        brand: "MXR",
+        type: ["all","delay"],
+        description: "Classic delay, ideal if you want to go for the safe option",
+        url_img: "./img/pedals/carboncopy.png",
+        price: 35000,
+    },
+    {
+        id:3,
+        name: "Chorus Ensemble CE-5",
+        brand: "Boss",
+        type: ["all", "chorus"],
+        description: "Classic pedal with beautiful angelic sounds",
+        url_img: "./img/pedals/ce5-boss.png",
+        price: 28000,
+    },
+    {
+        id:4,
+    },
+    {
+        id:5,
+    },
+    {
+        id:6,
+    },
+    {
+        id:7,
+    },
+    {
+        id:8,
+    },
+    {
+        id:9,
+    },
+    {
+        id:10,
+    },
+    {
+        id:11,
+    },
+    {
+        id:12,
+    },
+    {
+        id:13,
+    },
+    {
+        id:14,
+    },
+    {
+        id:15,
+    },
+    {
+        id:16,
+    },
+    {
+        id:17,
+    },
+    {
+        id:18,
+    },
+    {
+        id:19,
+    },
+    {
+        id:20,
+    },
+    {
+        id:21,
+    },
+    {
+        id:22,
+    },
+    {
+        id:23,
+    },
+    {
+        id:24,
+    },
+    {
+        id:25,
+    },
+    {
+        id:26,
+    },
+    {
+        id:27,
+    },
+    {
+        id:28,
+    },
+    {
+        id:29,
+    },
+    {
+        id:30,
+    },
+    {
+        id:31,
+    },
+    {
+        id:32,
+    },
+]
