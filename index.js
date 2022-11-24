651f4b2495f61ae5037d00e49bb23faa189864c2
RamiroPerez <ramiperez1997@hotmail.com>
2022-11-24 10:26:56 -0300

    agregadas cards

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const navbar = document.getElementById("navbar")
 
 //containers
 const categoryContainer = document.getElementById("category-section")
+const productsContainer = document.getElementById("products-container")
 
 function toggleNavbar(){
     navbar.classList.toggle("show-navbar");
@@ -38,11 +39,9 @@ function loadCategories(Categories){
 function removeSelectedCategory(){
     console.log(categoryCards)
     categoryCards.forEach(card =>{
-        console.log(card)
         if(card.classList.contains("selected")){
             card.classList.remove("selected");
         }
-        console.log("no ingreso a la funcion")
     })
 }
 
@@ -62,11 +61,58 @@ function selectCategory(event){
     setStyleOfSelectedCategory(selectedCategoryCard);
 }
 
+/*----------------Renderización de elementos--------------- */
+
+function createFxTags(arrayFx){
+    const tags = arrayFx.map(fx =>{
+        let htmlComponent = ""
+        const effect = fx.toString().toUpperCase()
+        if(effect != "ALL"){
+            htmlComponent = `<div class="tag ${effect}-style">${effect}</div>`;
+        }
+        return htmlComponent
+    }).join("");
+    return tags
+}
+
+function createHtmlProductCard(objProduct){
+    const {id, name, brand, fxs, description, url_img, price, free_shipping, stock} = objProduct;
+    return `
+    <div class="product-card">
+        ${free_shipping?`<div class="shipping-img" style="background-image: url('./img/utils/free-shipping.png')"></div>`:""}
+        <div class="product-img" style="background-image: url('${url_img}')"></div>
+        <div class="product-info">
+            <h3 class="product-name gradient-text">${name}</h3>
+            <span class="product-brand">${brand}</span>
+            <div class="fx-container">
+                ${createFxTags(fxs)}
+            </div>
+            <div class="description-toggle">
+                <p class="description-title">Description</p>
+                <div class="icon-arrow-down" style="background-image: url('./img/utils/icon-arrow-down.svg')"></div>
+            </div>
+            <p class="product-description">${description}</p>
+            <div class="separator"></div>
+            <div class="price-info-container">
+                <span class="product-price">$ ${price}</span>
+                <button class="btn-style-dark" data-id="${id}">Add</button>
+            </div>  
+        </div>
+    </div>
+    `;
+}
+
+function renderProducts(){
+    const html = Pedals.map(createHtmlProductCard).join("");
+    productsContainer.innerHTML = html;
+}
+
 function init(){
     window.addEventListener("DOMContentLoaded", function(){
         loadCategories(Categories);
         categoryCards = document.getElementsByClassName("category-card");
         categoryCards = [...categoryCards] //convierto a array de elementos
+        renderProducts();
     })
 
     barsMenuBtn.addEventListener("click", toggleNavbar);
