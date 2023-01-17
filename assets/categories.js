51b2b4ec532655925d6233b9d521cc21b2c3e494
RamiroPerez <ramiperez1997@hotmail.com>
2023-01-17 09:33:08 -0300

    ordenadas las carpetas

---
+++ assets/categories.js
@@ -0,0 +1,42 @@
+const Categories = [
+    {
+        id: 1,
+        name: "All",
+        url_img: "./assets/img/categories/c-all.png" 
+    },
+    {
+        id: 2,
+        name: "Compressor",
+        url_img: "./assets/img/categories/c-compressor.png" 
+    },
+    {
+        id: 3,
+        name: "Overdrive",
+        url_img: "./assets/img/categories/c-overdrive.png" 
+    },
+    {
+        id: 4,
+        name: "Distortion",
+        url_img: "./assets/img/categories/c-distortion.png" 
+    },    
+    {
+        id: 5,
+        name: "Modulation",
+        url_img: "./assets/img/categories/c-modulation.png" 
+    },
+    {
+        id: 6,
+        name: "Delay",
+        url_img: "./assets/img/categories/c-delay.png" 
+    },
+    {
+        id: 7,
+        name: "Reverb",
+        url_img: "./assets/img/categories/c-reverb.png" 
+    },
+    {
+        id: 8,
+        name: "Others",
+        url_img: "./assets/img/categories/c-others.png" 
+    },
+]
